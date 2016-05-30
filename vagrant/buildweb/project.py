1ccdf60b91c7b5a7dc3d34a61baf664b7a99f3bc
Scott Burns <scott@lentigo.net>
2016-05-29 22:29:30 -0400

    Add routes with arguments

--- vagrant/buildweb/project.py
+++ vagrant/buildweb/project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -17,7 +17,7 @@ session = DBSession()
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-    output = ''
+    output = '<h1>Menu Items: {}</h1>'.format(restaurant.name)
     for i in items:
         output += i.name
         output += '</br>'
@@ -29,20 +29,17 @@ def restaurantMenu(restaurant_id):
     return output
 
 # Task 1: Create route for newMenuItem function here
-
-
+@app.route('/restaurants/<int:restaurant_id>/new/')
 def newMenuItem(restaurant_id):
     return "page to create a new menu item. Task 1 complete!"
 
 # Task 2: Create route for editMenuItem function here
-
-
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
     return "page to edit a menu item. Task 2 complete!"
 
 # Task 3: Create a route for deleteMenuItem function here
-
-
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
     return "page to delete a menu item. Task 3 complete!"
 
