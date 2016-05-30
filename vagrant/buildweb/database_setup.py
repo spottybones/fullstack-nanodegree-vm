9547c16fe38391e9a62a7a4ae83d37aaa0e5d028
Scott Burns <scott@lentigo.net>
2016-05-30 10:06:49 -0400

    Add object serializers and JSON endpoints

--- vagrant/buildweb/database_setup.py
+++ vagrant/buildweb/database_setup.py
@@ -14,6 +14,14 @@ class Restaurant(Base):
     name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'name': self.name,
+            'id': self.id
+        }
+
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
@@ -25,6 +33,17 @@ class MenuItem(Base):
 
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+
+    @property
+    def serialize(self):
+        # Returns object data in easily serializable format
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
 
 
 engine = create_engine('sqlite:///restaurantmenu.db')
--- vagrant/buildweb/project.py
+++ vagrant/buildweb/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -73,6 +73,19 @@ def deleteMenuItem(restaurant_id, menu_id):
     else:
         return render_template('deletemenuitem.html', restaurant=restaurant,
                                item=item)
+
+# API endpoints
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON/')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON/')
+def menuItemJSON(restaurant_id, menu_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    item = session.query(MenuItem).filter_by(id=menu_id).one()
+    return jsonify(MenuItem=item.serialize)
 
 
 if __name__ == '__main__':
