bf30fb21b31421f229dede41b32a81d1671c7454
Scott Burns <scott@lentigo.net>
2016-05-29 22:08:38 -0400

    Flask project template

--- vagrant/buildweb/project.py
+++ vagrant/buildweb/project.py
@@ -1,29 +1,50 @@
 from flask import Flask
-app = Flask(__name__)
-
-# set up a database session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
+app = Flask(__name__)
+
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
+
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-@app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    restaurant = session.query(Restaurant).first()
-    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-    output = "<h1>Menu Items at {}</h1>".format(restaurant.name)
-    for item in items:
-        output += "<p>{}<br>".format(item.name)
-        output += "{}<br>".format(item.price)
-        output += "{}</p>\n".format(item.description)
 
+@app.route('/')
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output += '</br>'
+        output += '</br>'
     return output
 
+# Task 1: Create route for newMenuItem function here
+
+
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+# Task 2: Create route for editMenuItem function here
+
+
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+
+
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':
     app.debug = True
